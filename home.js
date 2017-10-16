c996ebf6ac7d4d534d740ec0ce9e4a7fbe084773
TaichiKitazawa <rad.1521.kt@gmail.com>
2017-10-16 14:04:08 +0900

    home update

--- home.js
+++ home.js
@@ -3,9 +3,16 @@
 $(function(){
   $('body').vegas({ //背景画像でスライドショーしたい場所の設定
     slides: [
-     { src: 'images/day1/day1_1.jpg' }, //スライドする画像を配列で設定
-     { src: 'images/day1/day1_2.jpg' },
-     { src: 'images/day1/day1_3.jpg' }
+     { src: 'images/day1/day1_3.jpg' }, //スライドする画像を配列で設定
+     { src: 'images/day1/day1_6.jpg' },
+     { src: 'images/day1/day1_9.jpg' },
+     { src: 'images/day1/day2_1.jpg' },
+     { src: 'images/day1/day1_3.jpg' },
+        { src: 'images/day1/day1_7.jpg' },
+        { src: 'images/day1/day1_8.jpg' },
+        { src: 'images/day1/day1_9.jpg' },
+        { src: 'images/day1/day1_15.jpg' },
+        { src: 'images/day1/day1_18.jpg' },
     ],
      delay: 5000, //スライドまでの時間ををミリ秒単位で設定
      timer: true, //タイマーバーの表示/非表示を切り替え
